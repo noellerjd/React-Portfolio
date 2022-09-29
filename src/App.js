6b54849c74cafce09c5c0b2813875aa8b4885f2d
noellerjd <noellerninja@gmail.com>
2022-09-28 18:22:53 -0600

    updated routes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Header />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/React-Portfolio" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
